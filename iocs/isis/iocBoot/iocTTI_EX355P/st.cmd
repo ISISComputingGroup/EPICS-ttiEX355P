298b3efe4b6e0398b8cedea41a7d21610a422f6d
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2014-07-22 14:01:42 +0000

    Update comments
    Former-commit-id: 59e612a73bdf231f821f72aacbb2fcde5d8ed352

--- iocs/isis/iocBoot/iocTTI_EX355P/st.cmd
+++ iocs/isis/iocBoot/iocTTI_EX355P/st.cmd
@@ -14,15 +14,15 @@ cd ${TOP}
 dbLoadDatabase "dbd/TTI_EX355P.dbd"
 TTI_EX355P_registerRecordDeviceDriver pdbbase
 
-drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)
-asynSetOption("L0", -1, "baud", "9600")
-asynSetOption("L0", -1, "bits", "8")
-asynSetOption("L0", -1, "parity", "none")
-asynSetOption("L0", -1, "stop", "1")
+#drvAsynSerialPortConfigure("L0", "$(TTY)", 0, 0, 0, 0)
+#asynSetOption("L0", -1, "baud", "9600")
+#asynSetOption("L0", -1, "bits", "8")
+#asynSetOption("L0", -1, "parity", "none")
+#asynSetOption("L0", -1, "stop", "1")
 
 ## Load record instances
 dbLoadRecords("$(TOP)/../../db/ttiEX355P.db","P=$(IOCNAME):,PORT=L0")
-dbLoadRecords("$(IOCSTATS_DB)","IOC=$(IOCNAME)")
+#dbLoadRecords("$(IOCSTATS_DB)","IOC=$(IOCNAME)")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
